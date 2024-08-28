662dd2035879d6a5e15eaf11e4fe1c87f464fdd6
mdadmin22 <marco.frencia@gmail.com>
2024-08-28 13:12:56 -0300

    nuevo archivo data

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+// data.ts
+
+export const relatedArtists = [
+    { name: 'Artist 1', id: 1, image: 'public/buddha-709861_1280.jpg' },
+    { name: 'Artist 2', id: 2, image: 'public/meditacion.jpeg' },
+    // Más artistas...
+  ];
+  
+  export const recommendedAlbums = [
+    { title: 'Album 1', id: 1, artist: 'Artist 1' },
+    { title: 'Album 2', id: 2, artist: 'Artist 2' },
+    // Más álbumes...
+  ];
+  
+  export const mostListenedTracks = [
+    { title: 'Track 1', id: 1, artist: 'Artist 1' },
+    { title: 'Track 2', id: 2, artist: 'Artist 2' },
+    // Más canciones...
+  ];
+  
+  // Más datos según sea necesario
+  
